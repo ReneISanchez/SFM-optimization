39224ff2ca2a4727930c8673b83e22826d952741
Alric Althoff <aalthoff@eng.ucsd.edu>
2015-04-21 19:28:04 +0000

    RichFeatureMatcher.cpp edited online with Bitbucket

--- SfMLib/src/features/RichFeatureMatcher.cpp
+++ SfMLib/src/features/RichFeatureMatcher.cpp
@@ -47,7 +47,6 @@
 
 using namespace std;
 using namespace cv;
-<<<<<<< HEAD
 
 static const unsigned char popCountTable[] =
 {
@@ -60,15 +59,12 @@ static const unsigned char popCountTable[] =
 	2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
 	3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8
 };
-=======
->>>>>>> 796fc0189b4b26a92c2dd753336c52943d143ad0
 
-static void getDescriptorDist(const cv::Mat& descriptors_1, 
-		const cv::Mat& descriptors_2,
-		std::vector<std::pair<int,int> >& minDist){
+/*
+static void getDescriptorDist(const cv::Mat& descriptors_1, const cv::Mat& descriptors_2, std::vector<std::pair<int,int> >& minDist) {
+
 	for (int x = 0; x < descriptors_1.rows; x++)
 	{
-<<<<<<< HEAD
 		int min = INT_MAX;
 		int yIdx = 0;
 		for (int y = 0 ; y < descriptors_2.rows ; y++)
@@ -86,9 +82,9 @@ static void getDescriptorDist(const cv::Mat& descriptors_1,
 		minDist.push_back(std::make_pair(yIdx, min));
 	}
 } //--endGetDescriptorDist
+*/
 
-/*
-static void getDescriptorDist(const cv::Mat& descriptors_1, const cv::Mat& descriptors_2, std::vector<std::pair<int,int> >& minDist){
+static void getDescriptorDist(const cv::Mat& descriptors_1, const cv::Mat& descriptors_2, std::vector<std::pair<int,int> >& minDist) {
 
     for (int x = 0; x < descriptors_1.rows; x++)
     {
@@ -193,7 +189,6 @@ static void getDescriptorDist(const cv::Mat& descriptors_1, const cv::Mat& descr
         minDist.push_back(make_pair(yIdx, min));
     }
 } //--endGetDescriptorDist
-*/
 
 static void verifyCrossCheck(vector<pair<int,int> >& minDist1, vector<pair<int,int> >& minDist2, vector < vector<DMatch> >& nn_matches) {
 	
@@ -222,154 +217,16 @@ static void verifyCrossCheck(vector<pair<int,int> >& minDist1, vector<pair<int,i
 
         nn_matches.push_back(tempVect);
     }
-=======
-	        		int min = INT_MAX;
-		int yIdx = 0;
-
-
-            __m128i mDes1_first, mDes1_sec, mDes2_first, mDes2_sec, c1, c2;
-            
-		for (int y = 0 ; y < descriptors_2.rows ; y++)
-		{
-		                int sumHamDist1 = 0;
-            int sumHamDist2 = 0;
-            int sumHamDist = 0;
-	
-			for (int z = 0; z < descriptors_1.cols; z = z + 16){
-                mDes1_first = _mm_set_epi8(descriptors_1.at<unsigned char>(x,z), 
-                        descriptors_1.at<unsigned char>(x,z+1), 
-                        descriptors_1.at<unsigned char>(x,z+2), 
-                        descriptors_1.at<unsigned char>(x,z+3), 
-                        descriptors_1.at<unsigned char>(x,z+4), 
-                        descriptors_1.at<unsigned char>(x,z+5), 
-                        descriptors_1.at<unsigned char>(x,z+6), 
-                        descriptors_1.at<unsigned char>(x,z+7), 
-                        descriptors_1.at<unsigned char>(x,z+8),
-                        descriptors_1.at<unsigned char>(x,z+9), 
-                        descriptors_1.at<unsigned char>(x,z+10), 
-                        descriptors_1.at<unsigned char>(x,z+11), 
-                        descriptors_1.at<unsigned char>(x,z+12), 
-                        descriptors_1.at<unsigned char>(x,z+13), 
-                        descriptors_1.at<unsigned char>(x,z+14), 
-                        descriptors_1.at<unsigned char>(x,z+15)); 
-
-                mDes1_sec = _mm_set_epi8(descriptors_1.at<unsigned char>(x,z+16), 
-                        descriptors_1.at<unsigned char>(x,z+17), 
-                        descriptors_1.at<unsigned char>(x,z+18), 
-                        descriptors_1.at<unsigned char>(x,z+19), 
-                        descriptors_1.at<unsigned char>(x,z+20), 
-                        descriptors_1.at<unsigned char>(x,z+21), 
-                        descriptors_1.at<unsigned char>(x,z+22), 
-                        descriptors_1.at<unsigned char>(x,z+23), 
-                        descriptors_1.at<unsigned char>(x,z+24), 
-                        descriptors_1.at<unsigned char>(x,z+25), 
-                        descriptors_1.at<unsigned char>(x,z+26), 
-                        descriptors_1.at<unsigned char>(x,z+27), 
-                        descriptors_1.at<unsigned char>(x,z+28), 
-                        descriptors_1.at<unsigned char>(x,z+29), 
-                        descriptors_1.at<unsigned char>(x,z+30), 
-                        descriptors_1.at<unsigned char>(x,z+31)); 
-
-                mDes2_first = _mm_set_epi8(descriptors_2.at<unsigned char>(y,z), 
-                        descriptors_2.at<unsigned char>(y,z+1), 
-                        descriptors_2.at<unsigned char>(y,z+2), 
-                        descriptors_2.at<unsigned char>(y,z+3),
-                        descriptors_2.at<unsigned char>(y,z+4), 
-                        descriptors_2.at<unsigned char>(y,z+5), 
-                        descriptors_2.at<unsigned char>(y,z+6), 
-                        descriptors_2.at<unsigned char>(y,z+7), 
-                        descriptors_2.at<unsigned char>(y,z+8),
-                        descriptors_2.at<unsigned char>(y,z+9), 
-                        descriptors_2.at<unsigned char>(y,z+10), 
-                        descriptors_2.at<unsigned char>(y,z+11), 
-                        descriptors_2.at<unsigned char>(y,z+12), 
-                        descriptors_2.at<unsigned char>(y,z+13), 
-                        descriptors_2.at<unsigned char>(y,z+14), 
-                        descriptors_2.at<unsigned char>(y,z+15)); 
-
-                mDes2_sec = _mm_set_epi8(descriptors_2.at<unsigned char>(y,z+16), 
-                        descriptors_2.at<unsigned char>(y,z+17), 
-                        descriptors_2.at<unsigned char>(y,z+18), 
-                        descriptors_2.at<unsigned char>(y,z+19), 
-                        descriptors_2.at<unsigned char>(y,z+20), 
-                        descriptors_2.at<unsigned char>(y,z+21), 
-                        descriptors_2.at<unsigned char>(y,z+22), 
-                        descriptors_2.at<unsigned char>(y,z+23), 
-                        descriptors_2.at<unsigned char>(y,z+24), 
-                        descriptors_2.at<unsigned char>(y,z+25), 
-                        descriptors_2.at<unsigned char>(y,z+26), 
-                        descriptors_2.at<unsigned char>(y,z+27), 
-                        descriptors_2.at<unsigned char>(y,z+28), 
-                        descriptors_2.at<unsigned char>(y,z+29), 
-                        descriptors_2.at<unsigned char>(y,z+30), 
-                        descriptors_2.at<unsigned char>(y,z+31)); 
-                
-                c1 = _mm_xor_si128(mDes1_first, mDes2_first);
-                c2 = _mm_xor_si128(mDes1_sec, mDes2_sec);
-                
-                sumHamDist1 = _mm_popcnt_u64(_mm_extract_epi64(c1,0) + _mm_popcnt_u64(_mm_extract_epi64(c1,1))); //1 shifts first half, 0 doesn't
-                sumHamDist2 = _mm_popcnt_u64(_mm_extract_epi64(c2,0) + _mm_popcnt_u64(_mm_extract_epi64(c2,1))); //1 shifts first half, 0 doesn't
-                sumHamDist = sumHamDist1 + sumHamDist2;
-			}
-			if (sumHamDist <= min)
-			{
-				min = sumHamDist;
-				yIdx = y;
-			}
-		}
-		minDist.push_back(std::make_pair(yIdx, min));
-	}
-} //--endGetDescriptorDist
-static void verifyCrossCheck(std::vector<std::pair<int,int> >&minDist1,
-		std::vector<std::pair<int,int> >&minDist2,
-		vector < vector<DMatch> >& nn_matches){
-	for (unsigned int x = 0; x < minDist1.size() && x < minDist2.size(); x++)
-		// size cutoff at smaller vector
-	{
-		std::vector<cv::DMatch> tempVect;
-		unsigned int tempDis1 = minDist2[ minDist1[x].first ].second;  // distances from <-
-		unsigned int tempDis2 = minDist1[ minDist2[x].first ].second; // distances from ->
-		if ( tempDis2 < tempDis1 )
-		{
-			/*
-			   --- minDistance1[x].second is the HamDist from checking all of descriptors_2
-			   against the first of descriptors_1, so we want the indices of the verified distance coming
-			   from descriptors_2.
-			   To locate it you only need which row it's in bc they all start at col 0 --- 
-			 */
-			tempVect.push_back( cv::DMatch (x , minDist1[x].first , (float)tempDis2));
-		}
-		else
-		{
-			//tempVect.push_back(DMatch( minDistance1[x].first, x, (float)tempDis1) ); //orig
-			tempVect.push_back(cv::DMatch( x, minDist1[x].first, (float)tempDis1) );
-		}
-		nn_matches.push_back(tempVect);
-		tempVect.clear();
-	}
->>>>>>> 796fc0189b4b26a92c2dd753336c52943d143ad0
 } //endVerifyCrossCheck
 /*---------------------------------------------------------------------------*/
 
 //c'tor
-<<<<<<< HEAD
 RichFeatureMatcher::RichFeatureMatcher(std::vector<cv::Mat>& imgs_, std::vector<std::vector<cv::KeyPoint> >& imgpts_) :
     imgs(imgs_), imgpts(imgpts_) 
-=======
-RichFeatureMatcher::RichFeatureMatcher(std::vector<cv::Mat>& imgs_,
-		std::vector<std::vector<cv::KeyPoint> >& imgpts_) :
-	imgs(imgs_), imgpts(imgpts_) 
->>>>>>> 796fc0189b4b26a92c2dd753336c52943d143ad0
 {
 	detector = FeatureDetector::create("PyramidFAST");
 	extractor = DescriptorExtractor::create("ORB");
-
-<<<<<<< HEAD
     std::cout << "-------------------- extract feature points for all images -------------------\n";
-=======
-	std::cout
-		<< " -------------------- extract feature points for all images -------------------\n";
->>>>>>> 796fc0189b4b26a92c2dd753336c52943d143ad0
 
 	detector->detect(imgs, imgpts);
 	extractor->compute(imgs, imgpts, descriptors);
@@ -382,7 +239,6 @@ void RichFeatureMatcher::MatchFeatures(int idx_i, int idx_j, vector<DMatch>* mat
 	const Mat& img_1 = imgs[idx_i];
 	const Mat& img_2 = imgs[idx_j];
 #endif
-<<<<<<< HEAD
     const vector<KeyPoint>& imgpts1 = imgpts[idx_i];
     const vector<KeyPoint>& imgpts2 = imgpts[idx_j];
     const Mat& descriptors_1 = descriptors[idx_i];
@@ -422,14 +278,13 @@ void RichFeatureMatcher::MatchFeatures(int idx_i, int idx_j, vector<DMatch>* mat
         //matcher.knnMatch(descriptors_1, descriptors_2, nn_matches, 1);
 		//nn_matches.clear();
         /*---------------------------------------------------------------------------*/
-	  ///*
+
         std::vector<std::pair<int,int> > minDistance1; // holds minDist from first check
         std::vector<std::pair<int,int> > minDistance2; // holds minDist from cross check
         getDescriptorDist(descriptors_1, descriptors_2, minDistance1); // returns minDistance1
         getDescriptorDist(descriptors_2, descriptors_1, minDistance2); // returns minDistance2
 		verifyCrossCheck(minDistance1, minDistance2, nn_matches);
 
-		//*/
         /*---------------------------------------------------------------------------*/
         matches->clear();
 
@@ -452,80 +307,12 @@ void RichFeatureMatcher::MatchFeatures(int idx_i, int idx_j, vector<DMatch>* mat
     double max_dist = 0;
     double min_dist = 0.0;
     cv::minMaxIdx(dists, &min_dist, &max_dist);
-=======
-	const vector<KeyPoint>& imgpts1 = imgpts[idx_i];
-	const vector<KeyPoint>& imgpts2 = imgpts[idx_j];
-	const Mat& descriptors_1 = descriptors[idx_i];
-	const Mat& descriptors_2 = descriptors[idx_j];
-
-	std::vector<DMatch> good_matches_, very_good_matches_;
-	std::vector<KeyPoint> keypoints_1, keypoints_2;
-
-	cout << "imgpts1 has " << imgpts1.size() << " points (descriptors "
-		<< descriptors_1.rows << ")" << endl;
-	cout << "imgpts2 has " << imgpts2.size() << " points (descriptors "
-		<< descriptors_2.rows << ")" << endl;
-
-	keypoints_1 = imgpts1;
-	keypoints_2 = imgpts2;
-
-	if (descriptors_1.empty()) 
-	{
-		CV_Error(0, "descriptors_1 is empty");
-	}
-	if (descriptors_2.empty()) 
-	{
-		CV_Error(0, "descriptors_2 is empty");
-	}
-
-	//matching descriptor vectors using Brute Force matcher
-	BFMatcher matcher(NORM_HAMMING, true); //allow cross-check
-	std::vector < DMatch > matches_;
-	if (matches == NULL) {
-		matches = &matches_;
-	}
-
-	vector<double> dists;
-	if (matches->size() == 0) 
-	{
-		vector < vector<DMatch> > nn_matches;
-		//matcher.knnMatch(descriptors_1, descriptors_2, nn_matches, 1);
-		/*---------------------------------------------------------------------------*/
-		std::vector<std::pair<int,int> > minDistance1; // holds minDist from first check
-		std::vector<std::pair<int,int> > minDistance2; // holds minDist from cross check
-		getDescriptorDist(descriptors_1, descriptors_2, minDistance1); // returns minDistance1
-		getDescriptorDist(descriptors_2, descriptors_1, minDistance2); // returns minDistance2
-		verifyCrossCheck(minDistance1, minDistance2, nn_matches);
-		/*---------------------------------------------------------------------------*/
-		matches->clear();
-
-		for (unsigned int i = 0; i < nn_matches.size(); i++) 
-		{
-			if (nn_matches[i].size() > 0) 
-			{
-				matches->push_back(nn_matches[i][0]);
-				double dist = matches->back().distance;
-				if (fabs(dist) > 10000)
-				{
-					dist = 1.0;
-				}
-				matches->back().distance = dist;
-				dists.push_back(dist);
-			}
-		}
-	}
-
-	double max_dist = 0;
-	double min_dist = 0.0;
-	cv::minMaxIdx(dists, &min_dist, &max_dist);
->>>>>>> 796fc0189b4b26a92c2dd753336c52943d143ad0
 
 #ifdef __SFM__DEBUG__
 	printf("-- Max dist : %f \n", max_dist );
 	printf("-- Min dist : %f \n", min_dist );
 #endif
 
-<<<<<<< HEAD
     vector<KeyPoint> imgpts1_good, imgpts2_good;
 
     if (min_dist < 10.0) 
@@ -561,85 +348,4 @@ void RichFeatureMatcher::MatchFeatures(int idx_i, int idx_j, vector<DMatch>* mat
     *matches = good_matches_;
 
     return;
-=======
-	vector<KeyPoint> imgpts1_good, imgpts2_good;
-
-	if (min_dist < 10.0) 
-	{
-		min_dist = 10.0;
-	}
-
-	// Eliminate any re-matching of training points (multiple queries to one training)
-	double cutoff = 4.0 * min_dist;
-	std::set<int> existing_trainIdx;
-	for (unsigned int i = 0; i < matches->size(); i++) 
-	{
-		//"normalize" matching: somtimes imgIdx is the one holding the trainIdx
-		if ((*matches)[i].trainIdx <= 0) 
-		{
-			(*matches)[i].trainIdx = (*matches)[i].imgIdx;
-		}
-
-		int tidx = (*matches)[i].trainIdx;
-		if ((*matches)[i].distance > 0.0 && (*matches)[i].distance < cutoff) 
-		{
-			if (existing_trainIdx.find(tidx) == existing_trainIdx.end()
-					&& tidx >= 0 && tidx < (int) (keypoints_2.size())) 
-			{
-				good_matches_.push_back((*matches)[i]);
-				//imgpts1_good.push_back(keypoints_1[(*matches)[i].queryIdx]);
-				//imgpts2_good.push_back(keypoints_2[tidx]);
-				existing_trainIdx.insert(tidx);
-			}
-		}
-	}
-
-	cout << "Keep " << good_matches_.size() << " out of " << matches->size()
-		<< " matches" << endl;
-
-	*matches = good_matches_;
-
-	return;
-
-#if 0
-#ifdef __SFM__DEBUG__
-	cout << "keypoints_1.size() " << keypoints_1.size() << " imgpts1_good.size() " << imgpts1_good.size() << endl;
-	cout << "keypoints_2.size() " << keypoints_2.size() << " imgpts2_good.size() " << imgpts2_good.size() << endl;
-
-	{
-		//-- Draw only "good" matches
-		Mat img_matches;
-		drawMatches( img_1, keypoints_1, img_2, keypoints_2,
-				good_matches_, img_matches, Scalar::all(-1), Scalar::all(-1),
-				vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-		//-- Show detected matches
-		imshow( "Feature Matches", img_matches );
-		waitKey(100);
-		destroyWindow("Feature Matches");
-	}
-#endif
-
-	vector<uchar> status;
-	vector<KeyPoint> imgpts2_very_good,imgpts1_very_good;
-
-	//Select features that make epipolar sense
-	GetFundamentalMat(imgpts1_good,imgpts2_good,imgpts1_very_good,imgpts2_very_good,good_matches_);
-
-	//Draw matches
-
-#ifdef __SFM__DEBUG__
-	{
-		//-- Draw only "good" matches
-		Mat img_matches;
-		drawMatches( img_1, keypoints_1, img_2, keypoints_2,
-				good_matches_, img_matches, Scalar::all(-1), Scalar::all(-1),
-				vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-		//-- Show detected matches
-		imshow( "Good Matches", img_matches );
-		waitKey(100);
-		destroyWindow("Good Matches");
-	}
-#endif
-#endif
->>>>>>> 796fc0189b4b26a92c2dd753336c52943d143ad0
 }
